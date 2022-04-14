045054f6cb29faaa1fed6c4d9b0847e99702533a
Khizr Abdul Ahad Shaikh <khizars949@gmail.com>
2022-04-14 18:40:52 +0530

    Variables Understood.

--- bin/main.dart
+++ bin/main.dart
@@ -1,3 +1,20 @@
 void main() {
-  print("Hello World");
+  // print("Hello Dart");
+
+  // # Variables
+  // ## Valid variable names
+  var name = "khizr";
+  var f_name = "aquib";
+  var address1 = "Dhule, Maharashtra";
+  var $money = 100;
+
+  // ## InValid variable names
+  // var 1person = "numan";
+  // var for = "Male";
+
+  // ## Printing Variables
+  print(name);
+  print(f_name);
+  print(address1);
+  print($money);
 }
