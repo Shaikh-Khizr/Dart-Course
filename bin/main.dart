4878e64f701fcd8a9750f5111421bf339d3fefce
Khizr Abdul Ahad Shaikh <khizars949@gmail.com>
2022-04-16 14:55:17 +0530

    int, double, String, bool and var data types Understood.

--- bin/main.dart
+++ bin/main.dart
@@ -3,18 +3,63 @@ void main() {
 
   // # Variables
   // ## Valid variable names
-  var name = "khizr";
-  var f_name = "aquib";
-  var address1 = "Dhule, Maharashtra";
-  var $money = 100;
+  // var name = "khizr";
+  // var f_name = "aquib";
+  // var address1 = "Dhule, Maharashtra";
+  // var $money = 100;
 
   // ## InValid variable names
   // var 1person = "numan";
   // var for = "Male";
 
   // ## Printing Variables
-  print(name);
-  print(f_name);
-  print(address1);
-  print($money);
+  // print(name);
+  // print(f_name);
+  // print(address1);
+  // print($money);
+
+  // # Built-in-Datatypes
+  // ## int
+  int cart_item = 10;
+  // print(cart_item);
+
+  // ## double
+  // double price = 10; // It will take this as 10.0
+  // double price = 10.50;
+  // print(price);
+
+  // ## String
+  // String city = "Pune";
+  // print(city);
+
+  // ## bool
+  // bool is_completed = true;
+  // print(is_completed);
+
+  // bool is_login = true;
+  // print(is_login);
+
+  // ## var
+  // var quantity = 100;
+  // print(quantity);
+
+  // var cost = 100.50;
+  // print(cost);
+
+  // var gender = "Male";
+  // print(gender);
+
+  // var is_full = true;
+  // print(is_full);
+
+  // ## Check Data Type
+  // print(cart_item.runtimeType);
+  // print(price.runtimeType);
+  // print(city.runtimeType);
+  // print(is_completed.runtimeType);
+  // print(is_login.runtimeType);
+  // print(quantity.runtimeType);
+  // print(cost.runtimeType);
+  // print(gender.runtimeType);
+  // print(is_full.runtimeType);
 }
